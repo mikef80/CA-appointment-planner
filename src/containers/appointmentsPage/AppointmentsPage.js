2eaf17e829f731a05a7e9b5f72a0aeed1e16af05
Mike Francis <mfrancis@clever-touch.com>
2022-09-29 13:28:59 +0100

    Step 7 completed down as far as 'Clear the form on submission'. Need to go back and address the issue of contacts duplicate checking - seems to only compare with last contact entry added

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = (props) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  const { appointments, contacts, addAppointments } = props;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    const appointment = { title: title, contact: contact, date: date, time: time };
+
+    addAppointments(appointment);
+    setTitle("");
+    setContact("");
+    setDate("");
+    setTime("");
+  };
+
+  const updateTitleHandler = (e) => {
+    const title = e.target.value;
+    setTitle(title);
+  };
+
+  const updateContactHandler = (e) => {
+    const contact = e.target.value;
+    setContact(contact);
+  };
+
+  const updateDateHandler = (e) => {
+    const date = e.target.value;
+    setDate(date);
+  }
+
+  const updateTimeHandler = (e) => {
+    const time = e.target.value;
+    setTime(time);
   };
 
   return (
